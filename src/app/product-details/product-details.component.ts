480f1fd755f6fea41281644b3a3dc6f763758f63
tane-git <tane.alexander.wilson@gmail.com>
2022-10-18 13:45:15 +1300

    add active routing data usage and piping

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+	product: Product | undefined;
 
-  constructor() { }
+	constructor(private route: ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
-
-}
+	ngOnInit() {
+		// First get the product id from the current route.
+		const routeParams = this.route.snapshot.paramMap;
+		const productIdFromRoute = Number(routeParams.get('productId'));
+	
+		// Find the product that correspond with the id provided in route.
+		this.product = products.find(product => product.id === productIdFromRoute);
+	}}
