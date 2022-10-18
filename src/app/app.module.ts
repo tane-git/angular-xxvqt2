01bc1c3f89b04540477de1cc329e07eb4bbde185
tane-git <tane.alexander.wilson@gmail.com>
2022-10-18 13:53:17 +1300

    restructure folders and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductAlertsComponent } from './components/product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   imports: [
--- src/app/components/product-alerts/product-alerts.component.ts
+++ src/app/components/product-alerts/product-alerts.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Product } from '../products';
+import { Product } from '../../products';
 
 @Component({
   selector: 'app-product-alerts',
